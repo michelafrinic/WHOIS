0ffefab844f53f3ff7785e42ca256a541cf7c76e
michel <michel.odou@afrinic.net>
2014-06-09 17:22:47 +0400

    Refactor ExcludedResources + unit test ExcludedResources now automatically excludes the ROOT (0/0 or 0::/0).

--- whois-commons/src/main/java/net/ripe/db/whois/common/iptree/ExcludedResources.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/iptree/ExcludedResources.java
@@ -17,19 +17,28 @@ import java.util.List;
  */
 @Component
 public class ExcludedResources {
-    private List<Ipv4Resource> ipv4ResourcesToExclude = new ArrayList<Ipv4Resource>();
-    private List<Ipv6Resource> ipv6ResourcesToExclude = new ArrayList<Ipv6Resource>();
+    private final Ipv4Resource V4ROOT = Ipv4Resource.parse("0/0");
+    private final Ipv6Resource V6ROOT = Ipv6Resource.parse("0::/0");
+
+    private List<Ipv4Resource> ipv4ResourcesToExclude = null;
+    private List<Ipv6Resource> ipv6ResourcesToExclude = null;
 
     @Autowired
     public ExcludedResources(
             @Value("${whois.inetnum.exclude.ipv4}") final String[] ipv4ParentDomainToExclude,
             @Value("${whois.inetnum.exclude.ipv6}") final String[] ipv6ParentDomainToExclude) {
-        for (int i = 0; i < ipv4ParentDomainToExclude.length; i++) {
-            ipv4ResourcesToExclude.add(Ipv4Resource.parse(ipv4ParentDomainToExclude[i]));
+        if(ipv4ParentDomainToExclude != null) {
+            ipv4ResourcesToExclude = new ArrayList<Ipv4Resource>();
+            for (int i = 0; i < ipv4ParentDomainToExclude.length; i++) {
+                ipv4ResourcesToExclude.add(Ipv4Resource.parse(ipv4ParentDomainToExclude[i]));
+            }
         }
 
-        for (int i = 0; i < ipv6ParentDomainToExclude.length; i++) {
-            ipv6ResourcesToExclude.add(Ipv6Resource.parse(ipv6ParentDomainToExclude[i]));
+        if(ipv6ParentDomainToExclude != null) {
+            ipv6ResourcesToExclude = new ArrayList<Ipv6Resource>();
+            for (int i = 0; i < ipv6ParentDomainToExclude.length; i++) {
+                ipv6ResourcesToExclude.add(Ipv6Resource.parse(ipv6ParentDomainToExclude[i]));
+            }
         }
     }
 
@@ -41,19 +50,33 @@ public class ExcludedResources {
         return ipv6ResourcesToExclude;
     }
 
-    private boolean isExcluded(Ipv4Entry entry) {
-        for(Ipv4Resource resource : ipv4ResourcesToExclude) {
-            if(resource.contains(entry.getKey())) {
+    public boolean isExcluded(Ipv4Entry entry) {
+        if(ipv4ResourcesToExclude != null && entry != null) {
+            // root is too wide a net -> excluded
+            if(entry.getKey().contains(V4ROOT)) {
                 return true;
+            }
+
+            for (Ipv4Resource resource : ipv4ResourcesToExclude) {
+                if (resource.contains(entry.getKey())) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    private boolean isExcluded(Ipv6Entry entry) {
-        for(Ipv6Resource resource : ipv6ResourcesToExclude) {
-            if(resource.contains(entry.getKey())) {
+    public boolean isExcluded(Ipv6Entry entry) {
+        if(ipv6ResourcesToExclude != null && entry != null) {
+            // root is too wide a net -> excluded
+            if(entry.getKey().contains(V6ROOT)) {
                 return true;
+            }
+
+            for (Ipv6Resource resource : ipv6ResourcesToExclude) {
+                if (resource.contains(entry.getKey())) {
+                    return true;
+                }
             }
         }
         return false;
---
+++ whois-commons/src/test/java/net/ripe/db/whois/common/iptree/ExcludedResourcesTest.java
@@ -0,0 +1,80 @@
+package net.ripe.db.whois.common.iptree;
+
+import net.ripe.db.whois.common.domain.Ipv4Resource;
+import net.ripe.db.whois.common.domain.Ipv6Resource;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by michel on 6/9/14.
+ */
+public class ExcludedResourcesTest {
+
+    @Test
+    public void nullLists() {
+        String [] excludedV4Resources = null;
+        String [] excludedV6Resources = null;
+
+        ExcludedResources excludedResources = new ExcludedResources(excludedV4Resources, excludedV6Resources);
+
+        Assert.assertFalse(excludedResources.isExcluded((Ipv4Entry) null));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("0/0"), 1)));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("11.100.0.0/16"), 1)));
+
+        Assert.assertFalse(excludedResources.isExcluded((Ipv6Entry) null));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv6Entry(Ipv6Resource.parse("0::/0"), 1)));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv6Entry(Ipv6Resource.parse("2c0f:f930::/32"), 1)));
+    }
+
+    @Test
+    public void emptyLists() {
+        String [] excludedV4Resources = {};
+        String [] excludedV6Resources = {};
+
+        ExcludedResources excludedResources = new ExcludedResources(excludedV4Resources, excludedV6Resources);
+
+        Assert.assertFalse(excludedResources.isExcluded((Ipv4Entry) null));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("0/0"), 1)));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("11.100.0.0/16"), 1)));
+
+        Assert.assertFalse(excludedResources.isExcluded((Ipv6Entry) null));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv6Entry(Ipv6Resource.parse("0::/0"), 1)));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv6Entry(Ipv6Resource.parse("2c0f:f930::/32"), 1)));
+    }
+
+    @Test
+    public void testV4Exclusions() {
+        String [] excludedV4Resources = {"196/8","197/8"};
+        String [] excludedV6Resources = null;
+
+        ExcludedResources excludedResources = new ExcludedResources(excludedV4Resources, excludedV6Resources);
+
+        Assert.assertFalse(excludedResources.isExcluded((Ipv4Entry) null));
+        Assert.assertTrue(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("0/0"), 1)));
+        Assert.assertFalse(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("11.100.0.0/16"), 1)));
+        Assert.assertTrue(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("196.100.15.0/24"), 1)));
+        Assert.assertTrue(excludedResources.isExcluded(new Ipv4Entry(Ipv4Resource.parse("196.100.1.56"), 1)));
+    }
+
+    @Test
+    public void testV4ExclusionsList() {
+        String [] excludedV4Resources = {"196/8","197/8"};
+        String [] excludedV6Resources = null;
+
+        ExcludedResources excludedResources = new ExcludedResources(excludedV4Resources, excludedV6Resources);
+
+        List<Ipv4Entry> list = new ArrayList<Ipv4Entry>();
+        list.add(new Ipv4Entry(Ipv4Resource.parse("0/0"), 1));
+        list.add(new Ipv4Entry(Ipv4Resource.parse("196.100.15.0/24"), 2));
+        list.add(new Ipv4Entry(Ipv4Resource.parse("11.100.0.0/16"), 3));
+        list.add(new Ipv4Entry(Ipv4Resource.parse("196.100.1.56"), 4));
+
+        List<Ipv4Entry> filteredList = excludedResources.removeV4Excluded(list);
+
+        Assert.assertEquals(filteredList.size(), 1);
+        Assert.assertEquals(filteredList.get(0), new Ipv4Entry(Ipv4Resource.parse("11.100.0.0/16"), 3));
+    }
+}
